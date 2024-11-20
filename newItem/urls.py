555583f6b6a1cb8c6e150c022de330b3c3e79881
Maike2961 <mayki.silva@hotmail.com>
2024-11-20 19:43:04 -0300

    feat: add novas urls

--- newItem/urls.py
+++ newItem/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CadastroNovoCarro
+from .views import CadastroNovoCarro, Detalhes
 
 app_name = 'carro'
 
 urlpatterns = [
-    path('novo_carro/', CadastroNovoCarro, name="novo_carro")
+    path('novo_carro/', CadastroNovoCarro, name="novo_carro"),
+    path('detalhes/<uuid:id>/', Detalhes, name='detalhes')
 ]
