177cb6cb4111a4cba172c1f25bfde736adaf3905
Maike2961 <mayki.silva@hotmail.com>
2024-11-20 19:41:30 -0300

    fix: corrigido field marca

--- newItem/admin.py
+++ newItem/admin.py
@@ -6,6 +6,6 @@ admin.site.register(Marca)
 
 @admin.register(Carro)
 class CarroAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'marca', 'chassi', 'cor', 'preco', 'tipo_combustivel', 'imagem', 'ano')
+    list_display = ('nome', 'marca_carro', 'chassi', 'cor', 'preco', 'tipo_combustivel', 'imagem', 'ano', 'criado_por')
 
 # Register your models here.
