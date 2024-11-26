c4b5b2ab3a49ea9d4243d6ba95b383af113f53a0
mayki <mayki.silva@hotmail.com>
2024-11-26 17:07:45 -0300

    feat: add script para mostrar marcas dos carros e qtds na tela

--- core/views.py
+++ core/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, redirect
-from .forms import CadastroForm
+from .forms import CadastroForm, FiltroMarcaForm
 from newItem.models import Carro
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
 
 
 @login_required
 def index(request):
-    
+    form = FiltroMarcaForm(request.GET or None)
     item = Carro.objects.all()
+    if form.is_valid():
+        marca = form.cleaned_data.get('Pesquisa')
+        if marca:
+            item = item.filter(nome__icontains=marca)
+            
+    items_agrupado = (item.values('marca_carro__marca_carro').annotate(count=Count('marca_carro')).order_by('marca_carro__marca_carro'))
+    print(items_agrupado)
     context = {
-        'item': item
+        'item': item,
+        'form': form,
+        'items_agrupado': items_agrupado
     }
     return render(request, "core/index.html", context)
 
