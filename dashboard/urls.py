fcee1a2f091aa2e25ce3cd4f263cb4cb6364e7b8
mayki <mayki.silva@hotmail.com>
2024-11-26 17:14:28 -0300

    feat: add url de remocao de carro

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, editar
+from .views import dashboard, editar, delete
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('editar/<uuid:id>/', editar, name='editar')
+    path('editar/<uuid:id>/', editar, name='editar'),
+    path('delete/<uuid:id>/', delete, name='delete')
 ]
