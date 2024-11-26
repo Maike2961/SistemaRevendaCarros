5e372c6d4131ed9c2cc2564e4c63e0fe7cb807b3
mayki <mayki.silva@hotmail.com>
2024-11-26 17:10:24 -0300

    fix: Realocação de model

--- newItem/models.py
+++ newItem/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from babel.numbers import format_currency
 import uuid
 
 class Base(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     criado_em = models.DateTimeField(auto_now_add=True)
-    criado_por = models.ForeignKey(User, related_name='usuario', on_delete=models.CASCADE)
-    Atualizado_em = models.DateTimeField(auto_now=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
 
 class Marca(Base):
     marca_carro = models.CharField(max_length=255)
@@ -33,13 +33,16 @@ class Carro(Base):
     marca_carro = models.ForeignKey(Marca, related_name="items", on_delete=models.CASCADE)
     cor = models.CharField("Cor", max_length=15)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    criado_por = models.ForeignKey(User, related_name='usuario', on_delete=models.CASCADE)
     tipo_combustivel = models.CharField(choices=TIPO_COMBUSTIVEL, max_length=20, default="GASOLINA")
     ano = models.IntegerField()
     imagem = models.ImageField(upload_to="media")
 
     def __str__(self):
         return f"{self.marca_carro}, {self.ano}"
+    
+    def preco_formatado(self):
+        return format_currency(self.preco, 'BRL', locale='pt_BR')
 
 
 
